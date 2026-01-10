b6cf4cf065d31ef8646015b82d6ded35216e450a
Ellielotn <ellieltongama@gmail.com>
2026-01-10 17:41:00 -0300

    dia_3-OK

---
+++ dia_3.js
@@ -0,0 +1,16 @@
+/*
+Data: 10.01.2026
+Nível: Fácil
+Nome: Ellielton
+
+    3-Faça um programa que pede um valor e mostre na tela se o valor é positivo ou negativo.
+*/
+let numero = 3;
+console.log(numero)
+if(numero === 0) {
+    console.log("O numero informado é neutro.");
+}else if(numero > 0) {
+    console.log("Positivo");
+}else{
+    console.log("Negativo");
+}
